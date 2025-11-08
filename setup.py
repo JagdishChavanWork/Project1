c69cfa970eb67eaa673e0ddcaba4ab5574f6dc5b
Jagdish Chavan <jagdishchavan.work@gmail.com>
2025-11-08 21:02:02 +0530

    update changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+
+
+setup(
+    name='Project1',
+    version='0.0.1',
+    author='Jagdish',
+    author_email='jagdishchavan.work@gmail.com',
+    package=find_packages(),
+    install_requires=get_requirements())
